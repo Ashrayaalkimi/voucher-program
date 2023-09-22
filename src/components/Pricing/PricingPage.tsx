9b6aeebaf7bba8b893416b31e02846b3c5f7f38d
Ashrayaa <mailtoashrayaa@gmail.com>
2023-09-22 22:51:50 +0530

    changed content of heading and paragraph

--- src/components/Pricing/PricingPage.tsx
+++ src/components/Pricing/PricingPage.tsx
@@ -27,11 +27,14 @@ const PricingPage = (props: Props) => {
     <>
       <div className="flex flex-col items-center gap-10 lg:gap-20 py-16 xl:justify-center">
         <div className="flex flex-col items-center gap-3 ">
-          <h1 className="text-5xl font-bold leading-[52px]">Vouchers</h1>
-          <p className="text-[#CACACA] text-sm font-light leading-6 text-center mx-3 lg:px-40 xl:px-72 2xl:px-[490px]">
-            Simplify your trading strategy with our intuitive screening tool.
+          <h1 className="text-5xl font-bold leading-[52px]">Alert packs</h1>
+          <p className="text-[#CACACA] text-sm font-light leading-6 text-center mx-3 lg:px-40 xl:px-72 2xl:px-[500px]">
+            {/* Simplify your trading strategy with our intuitive screening tool.
             Whether you&apos;re a seasoned pro or just starting out, our tool
-            makes it easy to screen for patterns, technicals, and more.
+            makes it easy to screen for patterns, technicals, and more.  */}
+            Feel free to get on with your day knowing you won&apos;t miss a great trading
+            opportunity. Our alert packs will notify you in real-time on your
+            terms.
           </p>
         </div>
         <div>

58aed9d968664f284409c2ceae77274678eadbcc
Ashrayaa <mailtoashrayaa@gmail.com>
2023-09-27 19:37:55 +0530

    plan card shimmer added

--- src/components/Pricing/PlanCard.tsx
+++ src/components/Pricing/PlanCard.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import ShimmerPlanCards from "./ShimmerPlanCards";
 
 interface Product {
   id: number;
@@ -21,6 +22,7 @@ type PlanCardProps = {
 const PlanCard = ({ onSelectPlan }: PlanCardProps) => {
   const router = useRouter();
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(
@@ -29,9 +31,12 @@ const PlanCard = ({ onSelectPlan }: PlanCardProps) => {
       .then((response) => response.json())
       .then((responseData) => {
         setProducts(responseData);
+        setLoading(false);
+        console.log("responsedata", responseData);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+        setLoading(false);
       });
   }, []);
   const handleViewPlanbyId = (id: any) => {
@@ -40,62 +45,70 @@ const PlanCard = ({ onSelectPlan }: PlanCardProps) => {
   };
 
   return (
-    <div className="flex flex-col mx-4 gap-10 lg:grid lg:grid-flow-row lg:grid-cols-3 lg:gap-12 cursor-pointer">
-      <div
-        // key={product.id}
-        className="group flex flex-col items-center p-5 gap-6 bg-[#242424] rounded-3xl shadow-md transform transition duration-500 hover:scale-105 lg:hover:scale-110"
-      >
-        <div className="bg-[#313131] rounded-[10px] group group-hover:bg-gradient-to-r group-hover:from-[#FADD62] group-hover:to-[#ff7337]">
-          <button className="py-1 px-4 bg-gradient-to-r font-medium from-[#FADD62] to-[#FF7337] text-transparent bg-clip-text group-hover:text-black">
-            View real-time patterns{" "}
-          </button>
-        </div>
+    <>
+      {loading ? (
+        <ShimmerPlanCards />
+      ) : (
+        <>
+          <div className="flex flex-col mx-4 gap-10 lg:grid lg:grid-flow-row lg:grid-cols-3 lg:gap-12 cursor-pointer">
+            <div className="group flex flex-col items-center p-5 gap-6 bg-[#242424] rounded-3xl shadow-md transform transition duration-500 hover:scale-105 lg:hover:scale-110">
+              <div className="bg-[#313131] rounded-[10px] group group-hover:bg-gradient-to-r group-hover:from-[#FADD62] group-hover:to-[#ff7337]">
+                <button className="py-1 px-4 bg-gradient-to-r font-medium from-[#FADD62] to-[#FF7337] text-transparent bg-clip-text group-hover:text-black">
+                  View real-time patterns{" "}
+                </button>
+              </div>
 
-        <h1 className="text-4xl font-semibold">Free</h1>
-        <p className="my-2 gap-3 text-center text-[#cacaca] text-sm font-light w-48">
-          View all pattern formations at any time series
-        </p>
-        <button
-          // onClick={() => handleViewPlanbyId(product.id)}
-          className="text-black w-full text-base font-medium py-3 px-16 lg:px-16 bg-white rounded-xl"
-        >
-          Proceed to payment
-        </button>
-      </div>
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="group flex flex-col items-center p-5 gap-3 bg-[#242424] rounded-3xl shadow-md transform transition duration-500 hover:scale-105 lg:hover:scale-110"
-        >
-          <div className="bg-[#313131] rounded-[10px] group group-hover:bg-gradient-to-r group-hover:from-[#FADD62] group-hover:to-[#ff7337]">
-            <button className="py-1 px-4 bg-gradient-to-r font-medium from-[#FADD62] to-[#FF7337] text-transparent bg-clip-text group-hover:text-black">
-              {product.noOfAlerts} alerts
-            </button>
+              <h1 className="text-4xl font-semibold">Free</h1>
+              <p className="my-2 gap-3 text-center text-[#cacaca] text-sm font-light w-48">
+                View all pattern formations at any time series
+              </p>
+              <button
+                // onClick={() => handleViewPlanbyId(product.id)}
+                className="text-black w-full text-base font-medium py-3 px-16 lg:px-16 bg-white rounded-xl"
+              >
+                Proceed to payment
+              </button>
+            </div>
+
+            {products.map((product) => {
+              const centPrice =
+                product.noOfAlerts !== 0
+                  ? (product.basePrice / product.noOfAlerts).toFixed(2)
+                  : 0;
+
+              return (
+                <div
+                  key={product.id}
+                  className="group flex flex-col items-center p-5 gap-3 bg-[#242424] rounded-3xl shadow-md transform transition duration-500 hover:scale-105 lg:hover:scale-110"
+                >
+                  <div className="bg-[#313131] rounded-[10px] group group-hover:bg-gradient-to-r group-hover:from-[#FADD62] group-hover:to-[#ff7337]">
+                    <button className="py-1 px-4 bg-gradient-to-r font-medium from-[#FADD62] to-[#FF7337] text-transparent bg-clip-text group-hover:text-black">
+                      {product.noOfAlerts} alerts
+                    </button>
+                  </div>
+
+                  <h1 className="text-4xl font-semibold">
+                    $ {product.basePrice}
+                  </h1>
+                  <p className="text-sm font-semibold leading-9">
+                    ({centPrice}¢ per alert)
+                  </p>
+                  <p className="my-2 gap-3 text-center text-[#cacaca] text-sm font-light w-[165px]">
+                    Telegram, Email & In-app real-time alerts.{" "}
+                  </p>
+                  <button
+                    onClick={() => handleViewPlanbyId(product.id)}
+                    className="text-black w-full text-base font-medium py-3 px-16 lg:px-16 bg-white rounded-xl"
+                  >
+                    Proceed to payment
+                  </button>
+                </div>
+              );
+            })}
           </div>
-
-          <h1 className="text-4xl font-semibold">
-            $ {product.basePrice}
-            {/* {product.currency}/mo */}
-          </h1>
-          <p className="text-sm font-semibold leading-9">(20¢ per alert)</p>
-          <p className="my-2 gap-3 text-center text-[#cacaca] text-sm font-light w-[165px]">
-            Telegram, Email & In-app real-time alerts.{" "}
-          </p>
-          {/* <ul className="my-2 flex flex-col gap-3 text-center text-[#cacaca]">
-            <li>Telegram</li>
-            <li>Email</li>
-            <li>In-app</li>
-            <li>Real time alerts</li>
-          </ul> */}
-          <button
-            onClick={() => handleViewPlanbyId(product.id)}
-            className="text-black w-full text-base font-medium py-3 px-16 lg:px-16 bg-white rounded-xl"
-          >
-            Proceed to payment
-          </button>
-        </div>
-      ))}
-    </div>
+        </>
+      )}
+    </>
   );
 };
 
--- src/components/Pricing/ShimmerPlanCards.tsx
+++ src/components/Pricing/ShimmerPlanCards.tsx
@@ -10,19 +10,18 @@ const ShimmerPlanCards = (props: Props) => {
         .map((e, index) => (
           <div
             key={e.id}
-            className="group flex flex-col items-center p-5 gap-6 bg-[#242424] rounded-3xl shadow-md transform transition duration-500 hover:scale-105 lg:hover:scale-110"
+            className="group lg:w-80 flex flex-col items-center p-5 gap-6 bg-[#242424] rounded-3xl shadow-md transform transition duration-500 hover:scale-105 lg:hover:scale-110"
           >
-            <div className="bg-[#FADD62] rounded-[10px] group group-hover:bg-gradient-to-r group-hover:from-[#FADD62] group-hover:to-[#ff7337]">
-              <button className="py-1 px-4 bg-gradient-to-r font-medium from-[#FADD62] to-[#FF7337] text-transparent bg-clip-text group-hover:text-black lg:px-12"></button>
+            <div className="bg-[#404242] mt-6 rounded-[10px] group group-hover:bg-gradient-to-r group-hover:from-[#FADD62] group-hover:to-[#ff7337]">
+              <button className="py-1 px-4 bg-[#242424] text-transparent bg-clip-text group-hover:text-black lg:px-12"></button>
             </div>
 
-            <h1 className="w-full h-6 bg-[#242424] rounded-sm px-16  py-3"></h1>
             <ul className="my-2 flex flex-col gap-3 text-center text-[#c1bebe]">
              {Array(3).fill("").map((e,index)=>(
                 <li key={e.id} className="bg-[#404242] rounded-lg w-full h-4 py-3 px-16 lg:px-24"></li>
              ))} 
             </ul>
-            <button className="text-black w-full h-6 text-base font-medium py-3 px-16 lg:px-16 bg-[#737575] rounded-lg"></button>
+            <button className="text-black w-full h-10 text-base font-medium py-3 px-16 lg:px-16 bg-[#737575] rounded-lg"></button>
           </div>
         ))}
     </div>
